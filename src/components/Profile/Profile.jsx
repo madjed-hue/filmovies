888932e6d22bd71160e7cd05dab34d4f5bad41be
fateh beddiaf <kentounmax@gmail.com>
2023-10-27 10:44:42 +0100

    ..

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,43 @@
-// import { useSelector } from "react-redux";
-// import { userSelector } from "../../features/auth";
+import { useSelector } from "react-redux";
+import { userSelector } from "../../features/auth";
+import { Box, Button, Typography } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 const Profile = () => {
-  // const { user, isAuthenticated } = useSelector(userSelector);
+  const { user, isAuthenticated } = useSelector(userSelector);
 
-  return <div>Profile</div>;
+  const favoriteMovies = [];
+  const watchlistMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = "/";
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies?.results?.length && !watchlistMovies?.results?.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>
+          Favorite Movies
+          {/* <RatedCards title="Favorite Movies" data={favoriteMovies} />
+        <RatedCards title="Watchlist" data={watchlistMovies} /> */}
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
