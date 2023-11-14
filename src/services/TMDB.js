d8e491c822f561d5e1b54b4f9c40f2a832241008
fateh beddiaf <kentounmax@gmail.com>
2023-11-14 16:38:56 +0100

    adjust queries

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const tmdbApiKey = import.meta.env.VITE_TMDB_API_KEY;
 
+// create the api queries
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
